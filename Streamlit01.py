7d5a72cbfecd5622bd0d932b68c8b443e69a96d5
pakpoom301247 <157553654+pakpoom301247@users.noreply.github.com>
2024-01-24 11:20:10 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -19,7 +19,7 @@ with col3:
 
 df=pd.read_csv("./data/iris.csv")    
  
- 
+
 if(st.button("เเสดงข้อมูลตัวอย่าง")):
     st.write(df.head(10)) 
     st.button("ไม่เเสดงข้อมูลตัวอย่าง")
@@ -38,4 +38,23 @@ if(st.button("แสดงกราฟแท่ง")):
     st.button("ไม่แสดงกราฟเเท่ง")
 else:
     st.button("ไม่แสดงกราฟเเท่ง")    
+
+
+import matplotlib.pyplot as plt
+
+# Pie 
+labels = 'sepal.width', 'sepal.length', 'petal.width', 'petal.length'
+x1=df['sepal.width'].mean()
+x2=df['sepal.length'].mean()
+x3=df['petal.width'].mean()
+x4=df['petal.length'].mean()
+sizes = [x1, x2, x3, x4]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+#ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(fig1)    
 
