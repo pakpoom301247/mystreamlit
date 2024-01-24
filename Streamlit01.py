3d9a87bf9627074b788e40928d9f8c4d4fa4714a
pakpoom301247 <157553654+pakpoom301247@users.noreply.github.com>
2024-01-24 10:05:33 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.title("การทดสอบสร้างเว็บด้วยPython")
 st.image("data.jpeg")
-st.header("การนำเสนอข้อมูลกราฟด้วย Python") 
+st.header("การนำเสนอข้อมูลกราฟด้วย Python")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+
+with col2:
+
+with col3:
+
