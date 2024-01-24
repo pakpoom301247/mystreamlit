f17a773ddb2a6e56efc3233604cd5dcfe3d71c67
pakpoom301247 <157553654+pakpoom301247@users.noreply.github.com>
2024-01-24 11:50:12 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -58,34 +58,9 @@ ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
 
 st.pyplot(fig1)
 
+if(st.button("แสดงข้อมูล")):
+    st.pyplot(fig1)
+    st.button("ไม่แสดงข้อมูล")
 
-
-
-
-
-
-
-
-
-
-
-import matplotlib.pyplot as plt
-if(st.button("เเสดงกราฟวงกลม")):
-    chart_data = pd.DataFrame(
-    {
-        labels = 'sepal.width', 'sepal.length', 'petal.width', 'petal.length'
-        x1=df['sepal.width'].mean()
-        x2=df['sepal.length'].mean()
-        x3=df['petal.width'].mean() 
-        x4=df['petal.length'].mean()
-        sizes = [x1, x2, x3, x4]
-        explode = (0, 0.1, 0, 0)  
-        fig1, ax1 = plt.subplots()
-        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-    st.pyplot(fig1)    
-    }
-)    
-    st.button("ไม่เเสดงกราฟวงกลม")
 else:
-    st.button("ไม่เเสดงกราฟวงกลม")    
+    st.button("ไม่แสดงข้อมูล")   
