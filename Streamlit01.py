93e94baa13114cfd026c5b51591d45ab10e9b2fd
pakpoom301247 <157553654+pakpoom301247@users.noreply.github.com>
2024-01-24 11:57:18 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -42,13 +42,16 @@ else:
 
 import matplotlib.pyplot as plt
 
-# Pie 
+# Pie
 labels = 'sepal.width', 'sepal.length', 'petal.width', 'petal.length'
+
 x1=df['sepal.width'].mean()
 x2=df['sepal.length'].mean()
 x3=df['petal.width'].mean()
 x4=df['petal.length'].mean()
+
 sizes = [x1, x2, x3, x4]
+
 explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
 fig1, ax1 = plt.subplots()
@@ -56,11 +59,9 @@ ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 #ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-st.pyplot(fig1)
-
 if(st.button("แสดงข้อมูล")):
     st.pyplot(fig1)
     st.button("ไม่แสดงข้อมูล")
 
 else:
-    st.button("ไม่แสดงข้อมูล")   
+    st.button("ไม่แสดงข้อมูล")
